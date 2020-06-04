0bd76a16a8f3220e252f52c5adaf1d61ad9830dc
theo-r <theo.rutter@gmail.com>
2020-06-04 16:45:19 +0100

    Update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reed",
-    version="0.0.1",
+    version="0.0.2",
     author="Theo Rutter",
     author_email="theo.rutter@gmail.com",
     description="Search for jobs using the Reed API in python.",
