489e29102d6e455007e6f3369e8e68a54f9ecc55
theo-r <theo.rutter@gmail.com>
2020-06-04 16:26:55 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='reed', version='1.0', packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="reed",
+    version="0.0.1",
+    author="Theo Rutter",
+    author_email="theo.rutter@gmail.com",
+    description="Search for jobs using the Reed API in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/theo-r/reed",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
